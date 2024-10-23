554e13a89d4a3476a21a7e2e617494218d59b772
henkkapoika <henriknerg@hotmail.com>
2024-10-23 13:41:02 +0300

    first

---
+++ src/index.php
@@ -0,0 +1,70 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header>
+        <div class="header-field">
+            <ul>
+                <li>
+                    <a href="localhost:8000/">
+                        <img src="images/bloghouse_logo.png" alt="Logo of the website">   
+                    </a>
+                </li>
+                <li>
+                    <a href="http://">User Page</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <section class="main-advert">
+            <h1>Placeholder</h1>
+        </section>
+        <section class="main-posts">
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+            <article>
+                <h2>Post Title</h2>
+                <p>Post Content</p>
+                <p>Post Author</p>
+                <p>Post Date</p>
+            </article>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
